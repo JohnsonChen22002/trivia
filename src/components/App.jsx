edcdfcbe292e1f865e6120f45d802cc91d503a54
Johnson Chen <johnsonchen12345@gmail.com>
2019-02-06 23:23:19 +0000

    ConnectingToFirebase

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,52 @@
 import React, { Component } from 'react';
+import {buildFirebase, getRandomQuestion} from '../clients/firebase';
 import '../css/App.css';
 import Question from './Question.jsx';
 import AnswerButton from './AnswerButton.jsx';
 import Reset from './RestButton';
-const question = 'What was the most popular game in 2018?'
-const answers = ["Fortnite", "Red Dead Redemption II", "Tetris", "Legend of Zelda: Breath of the Wild"];
+const firebaseDatabase = buildFirebase();
+// const question = 'What was the most popular game in 2018?'
+// const answers = ["Fortnite", "Red Dead Redemption II", "Tetris", "Legend of Zelda: Breath of the Wild"];
  
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.updateState();
+    
+    this.state = {
+      questions : {},
+      currentQuestion: {
+        question_text: 'Question',
+        choices: [],
+        correct_choice_index: null,
+      }
+    }
+  }
+  
+  updateState(){
+    firebaseDatabase.ref('/questions').on('value', (snapshot)=> {
+      const question = getRandomQuestion(snapshot.val()); 
+      console.log(question);
+      const nextChoices = question.choices;
+      this.setState({
+        currentQuestion: {
+          choices: nextChoices,
+        }
+      })
+    });
 
+    
+  }
+  
   render() {
     return (
       <div className="app">
         Trivia!
-        <Question questiontext={question}/>
+        <Question questiontext={this.state.currentQuestion.question_text}/>
         {
-          answers.map(function(answer){
+          this.state.currentQuestion.choices.map(function(answer){
             return <AnswerButton  answertext={answer}/>;
           })
         }
